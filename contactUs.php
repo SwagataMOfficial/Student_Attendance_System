22c0a142c32e8d61cd78fa724807a36bff4e630f
SwagataMOfficial <swagatamukherjee365@gmail.com>
2023-10-19 21:44:11 +0530

    updated contact us ui

--- contactUs.php
+++ contactUs.php
@@ -1,5 +1,4 @@
 <?php
-// INSERT INTO `contact_us` (`sno`, `name`, `phone_number`, `email`, `user_concern`, `query_time`) VALUES (NULL, 'Swagata Mukherjee', '1234567890', 'email@email.com', 'your website is not working properly, please fix it as soon as possible.', current_timestamp());
 require("partitions/_dbconnect.php");
 
 // utility variables
@@ -22,103 +21,99 @@ if (isset($_POST['sent']) && $_POST['sent'] == "sent") {
 ?>
 
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contact Us</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Contact Us</title>
+	<link rel="stylesheet" href="css/contact_us.css" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg bg-dark mb-1">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="https://getbootstrap.com/docs/5.3/getting-started/introduction/">
-                <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30"
-                    height="24" class="d-inline-block align-text-top">
-                Bootstrap
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="http://localhost/Minor_Project/Student_Attendance_System/">Home</a>
-                    </li>
-                    <li class="nav-item" style="cursor: not-allowed;">
-                        <a class="nav-link disabled" aria-disabled="true" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="contactUs.php">Contact Us</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Other Options
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="loginStudent.php">Login as Student</a></li>
-                            <li><a class="dropdown-item" href="loginTeacher.php">Login as Teacher</a></li>
-                            <li><a class="dropdown-item" href="registerStudent.php">Sign Up as Student</a></li>
-                            <li><a class="dropdown-item" href="registerTeacher.php">Sign Up as Teacher</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="setup.php">Environment Set Up</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search...." aria-label="Search">
-                    <button class="btn btn-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
-    <div class="container mt-3">
-        <h1 class="text-center fw-bold">Contact Us</h1>
-        <form method="post">
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Name</label>
-                <input type="text" class="form-control" name="name" placeholder="Enter Your Name Here...."
-                    id="exampleInputPassword1">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Phone</label>
-                <input type="tel" class="form-control" name="phone" placeholder="Enter Your Phone Number Here...."
-                    id="exampleInputPassword1">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email Address</label>
-                <input type="email" class="form-control" name="email" id="exampleInputEmail1"
-                    placeholder="Enter Your Email Address Here...." aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="floatingTextarea" class="form-label">Your Concern</label>
-                <textarea class="form-control" name="message" placeholder="Enter Your Concern Here...."
-                    id="floatingTextarea"></textarea>
-            </div>
-            <button type="submit" name="sent" value="sent" class="btn btn-primary">Submit</button>
-        </form>
-        <?php
-        if ($sent) {
-            echo '<div class="mt-3 alert alert-success alert-dismissible fade show" role="alert">
-                <strong>Success!</strong> Message sent successfully!<br>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>';
-        }
-        ?>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-        crossorigin="anonymous"></script>
+	<header>
+		<nav>
+			<div><a href="index.php" class="nav-logo">Classified.in</a></div>
+			<div class="nav-lists">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">Docs</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="contactUs.php">Contact Us</a></li>
+				</ul>
+			</div>
+			<div class="buttons">
+				<button class="btn" id="loginBtn">Login</button>
+				<button class="btn" id="signUpBtn">Sign Up</button>
+			</div>
+		</nav>
+	</header>
+	<main>
+		<div class="login-signup">
+			<span role="button" class="material-symbols-outlined" id="closeBtn">close</span>
+			<div class="login-options">
+				<a href="loginStudent.php">Login as Student</a>
+				<a href="loginTeacher.php">Login as Teacher</a>
+			</div>
+			<div class="signup-options">
+				<a href="registerStudent.php">Sign up as Student</a>
+				<a href="registerTeacher.php">Sign up as Teacher</a>
+			</div>
+		</div>
+		<div class="container">
+			<div class="content">
+				<div class="image-box">
+					<img src="assets/contact.png" alt="background image" />
+				</div>
+				<form method="post" autocomplete="off">
+					<div class="topic">Enter Your Concern Below</div>
+					<div class="input-box">
+						<input type="text" name="name" id="name" required />
+						<label for="name">Enter your Full Name</label>
+					</div>
+					<div class="input-box">
+						<input type="tel" name="phone" id="phone" required />
+						<label for="phone">Enter your Phone Number</label>
+					</div>
+					<div class="input-box">
+						<input type="email" name="email" id="email" required />
+						<label for="email">Enter your Email ID</label>
+					</div>
+					<div class="message-box">
+						<textarea name="message" id="message"></textarea>
+						<label for="message">Enter your Message</label>
+					</div>
+					<div class="input-box">
+						<button type="submit" name="sent" value="sent">Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</main>
+	<script>
+		const loginBtn = document.getElementById('loginBtn');
+		const signUpBtn = document.getElementById('signUpBtn');
+		const closeBtn = document.getElementById('closeBtn');
+
+		closeBtn.addEventListener('click', ()=>{
+			document.querySelector('.login-signup').style.display = 'none';
+		});
+
+		loginBtn.addEventListener('click', () => {
+			document.querySelector('.login-signup .signup-options').style.display = 'none';
+			document.querySelector('.login-signup').style.display = 'block';
+			document.querySelector('.login-signup .login-options').style.display = 'flex';
+			
+		});
+		
+		signUpBtn.addEventListener('click', () => {
+			document.querySelector('.login-signup .login-options').style.display = 'none';
+			document.querySelector('.login-signup').style.display = 'block';
+			document.querySelector('.login-signup .signup-options').style.display = 'flex';
+		});
+	</script>
 </body>
 
 </html>
