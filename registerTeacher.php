43fca488eb3249c02a07541cd07c9829352f00d0
SwagataMOfficial <swagatamukherjee365@gmail.com>
2023-10-25 22:02:36 +0530

    important changes

--- registerTeacher.php
+++ registerTeacher.php
@@ -21,8 +21,8 @@ function sendMail($email, $newOTP)
         $mail->isSMTP(); //Send using SMTP
         $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
         $mail->SMTPAuth = true; //Enable SMTP authentication
-        $mail->Username = 'attendancesystem24x7@gmail.com'; //SMTP username
-        $mail->Password = 'ymfu zmou nlvr dwjx'; //SMTP password
+        $mail->Username = ''; //SMTP username
+        $mail->Password = ''; //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
         $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
